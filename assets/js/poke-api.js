a51a51784b9da45dc4d04af6d66f7738fa77a245
rhaneyko <rhaneykohonorio@gmail.com>
2024-11-10 12:41:35 -0400

    map details pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -11,10 +11,7 @@ pokeApi.getPokemons = (offset = 0, limit = 0) => {
     .then((jsonBody) => jsonBody.results)
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonsDetails) => {
-      debugger;
-      console.log(pokemonsDetails);
-    });
+    .then((pokemonsDetails) => pokemonsDetails);
 };
 
 Promise.all([fetch("")]).then((results) => {
